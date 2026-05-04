5b399906ad14701e38d9e3a160ebb1634002f24f
scamlett <scamlett@student.42malaga.com>
2026-05-04 16:03:28 +0200

    corrijo struct

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: scamlett <scamlett@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 15:15:20 by scamlett          #+#    #+#             */
-/*   Updated: 2026/05/04 15:56:37 by scamlett         ###   ########.fr       */
+/*   Updated: 2026/05/04 16:03:08 by scamlett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,10 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef enum e_move
-{
-	MOVE_SA,
-	MOVE_SB,
-	MOVE_SS,
-	MOVE_PA,
-	MOVE_PB,
-	MOVE_RA,
-	MOVE_RB,
-	MOVE_RR,
-	MOVE_RRA,
-	MOVE_RRB,
-	MOVE_RRR
-} 	t_move;
-
 typedef struct s_node
 {
-	t_move			move;
-	struct s_node	*next;
+    int				value;
+    struct s_node	*next;
 } 	t_node;
 
 typedef struct s_stack
